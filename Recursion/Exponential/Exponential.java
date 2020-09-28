653616312fd92e8b2e8127bbe1d7e98e96074919
konman01 <konimanjunatha@gmail.com>
2020-09-28 00:36:27 -0700

    Added the 2nd method to calculate the exponent is a number

--- Recursion/Exponential/Exponential.java
+++ Recursion/Exponential/Exponential.java
@@ -17,7 +17,7 @@ public class Exponential {
 	}// end of function
 	
 	// Function to calculate the Exponential Recursively
-	public int calExponentRecursive(int base, int exp) {
+	public int calExponentRecursive1(int base, int exp) {
 		
 		
 		// 1. Base case
@@ -28,8 +28,31 @@ public class Exponential {
 		}
 		
 		// recursive case 
-		return base * (calExponentRecursive(base, exp - 1));
+		return base * (calExponentRecursive1(base, exp - 1));
 		
+		
+	}// end of function
+	
+	
+	// Recursive Method 2
+	public int calExponentRecursive2(int base, int exp) {
+		
+		// 1. Base Case
+		// if exp == 0, return 1
+		if(exp == 0) {
+			return 1;
+		}
+		
+		// Recursive Case
+		// calculate the half
+		int half = calExponentIteration(base, exp/2);
+		
+		// exp is even. then return half * half
+		if(exp % 2 == 0) {
+			return half * half;
+		}
+		
+		return base * half * half;
 		
 	}// end of function
 	
@@ -40,4 +63,6 @@ public class Exponential {
 // 1. if exp is 1
 // 2. if exp is 0
 // 3. if exp > 1
+// 4. if exp is odd
+// 5. if exp is even
 
